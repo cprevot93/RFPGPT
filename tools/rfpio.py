e61746eba53a179a2d405ee0d549beb9f41891b1
Charles Prevot <charles.prevot8@gmail.com>
2023-03-19 22:24:19 +0100

    [TOOL] improve RFPio

--- tools/rfpio.py
+++ tools/rfpio.py
@@ -162,7 +162,7 @@ def get_reponses_head():
     exit(0)
 
 
-def get_reponses(_token: str, query: str, product_tags: list) -> list:
+def get_reponses(_token: str, query: str, product_tags: list, limit=3, search_only_in_question=False) -> list:
     """
     RFPIO API call
     """
@@ -178,7 +178,8 @@ def get_reponses(_token: str, query: str, product_tags: list) -> list:
     for cookie in cookies:
         driver.add_cookie(cookie)
 
-    limit = 5  # number of results
+    fields = ["question", "alternate_questions"] if search_only_in_question else [
+        "question", "alternate_questions", "answer"]
 
     data = {
         "term": query,
@@ -301,7 +302,7 @@ def get_reponses(_token: str, query: str, product_tags: list) -> list:
         'POST', 'https://app.rfpio.com/rfpserver/content-library/search', json=data, headers={'Authorization': f'Bearer {_token}'})
     driver.close()
 
-    if response.status_code == 403:
+    if response.status_code == 403 or response.status_code == 440:
         login()
     elif response.status_code != 200:
         log.error(
@@ -318,22 +319,27 @@ def get_reponses(_token: str, query: str, product_tags: list) -> list:
     return _j["results"]
 
 
-def strip_html_tags(text: str) -> str:
+def strip_html_tags_and_url(text: str) -> str:
     """Remove html tags from a string"""
     import re
-    clean = re.compile('<.*?>')
-    return re.sub(clean, '', text)
+    tags = re.compile(r'<.*?>|For more information:')
+    links = re.compile(
+        r'\(?https?:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*\)?')
+    clean_text = re.sub(tags, ' ', text)
+    clean_text = re.sub(links, ' ', clean_text)
+    return re.sub(' +', ' ', clean_text).strip()
 
 
 def format_response(results: list) -> list[str]:
     res = []
-    for r in results:
-        for a in r['answers']:
+    for i, r in enumerate(results):
+        for j, a in enumerate(r['answers']):
+            key = f"{a['key']}." if a['key'] != 'Response' else ""
             if a['type'] == 'RICH_TEXT':
-                answer = strip_html_tags(a['value'])
+                answer = strip_html_tags_and_url(a['value'])
             else:
                 answer = a['value']
-            answer = a['key'] + ': ' + answer
+            answer = f"Question: {strip_html_tags_and_url(r['question'])}\nResponse: {key} {answer}"
             res.append(answer)
     return res
 
@@ -341,7 +347,7 @@ def format_response(results: list) -> list[str]:
 class RFPIO(BaseTool):
     """Use RFPio to search for answers for Fortinet product."""
     name = "RFPio Search"
-    description = "Use this in addition to the normal search if the question pertains to in-depth Fortinet product functionalities. Inputs of this tool should start with Fortinet product name (no abbreviation) followed by a comma then the query. For example, `FortiGate,SD-WAN routing` would be the input if you wanted to search for SD-WAN on Fortigate."
+    description = "Use this in addition to the normal search if the question pertains to in-depth Fortinet product functionalities. Inputs of this tool should start with Fortinet product name (no abbreviation) followed by a comma then the query. Queyr MUST be in english. For example, `FortiGate,SD-WAN routing` would be the input if you wanted to search for SD-WAN on Fortigate."
 
     def __init__(self, *args, **kwargs):
         """Initialize the tool."""
@@ -381,6 +387,10 @@ if __name__ == '__main__':
 
     if query == "test":
         get_reponses_head()
+
+    if query == "clean":
+        print(strip_html_tags_and_url(sys.argv[2]))
+        exit(0)
 
     results = []
     if os.path.exists(TEST_SAMPLE_PATH):
